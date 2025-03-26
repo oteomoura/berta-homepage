ebeb8f3100f526d5db7dc4e4832026e5d3de3681
Teógenes Moura <mourateogenes@gmail.com>
2025-03-26 15:30:51 -0300

    wip

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['images.unsplash.com', 'plus.unsplash.com']
+    unoptimized: true  // This is needed for static export
   },
 };
 
