df811c163c5834f299964bda5b942d3f2dbc9fb4
Teógenes Moura <mourateogenes@gmail.com>
2025-03-26 15:36:40 -0300

    fix deploy

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'export',
+  basePath: '/berta-homepage',
   images: {
     unoptimized: true  // This is needed for static export
   },
